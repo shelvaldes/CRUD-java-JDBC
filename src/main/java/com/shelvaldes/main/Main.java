e82f3530ef11d9e69ca134823a5b1e7f1f15d9e6
Shel Valdés <hola@shelvaldes.com>
2024-05-09 18:10:13 -0600

    Interfaz gráfica

--- src/main/java/com/shelvaldes/main/Main.java
+++ src/main/java/com/shelvaldes/main/Main.java
@@ -1,34 +1,13 @@
 package com.shelvaldes.main;
 
-import com.shelvaldes.models.Employee;
-import com.shelvaldes.repository.EmployeeRepository;
-import com.shelvaldes.repository.Repository;
-import com.shelvaldes.utils.DatabaseConnection;
+import com.shelvaldes.view.SwingApp;
 
-import java.sql.Connection;
-import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args){
 
-        try ( Connection myConn = DatabaseConnection.getInstance()){
-            Repository<Employee> repository = new EmployeeRepository();
+        SwingApp app  = new SwingApp();
+        app.setVisible(true);
 
-            System.out.println("--Listando--");
-            repository.findAll().forEach(System.out::println);
-
-
-            System.out.println("--Empleado insertado--");
-            repository.findAll().forEach(System.out::println);
-
-            repository.delete(10);
-
-            System.out.println("Empleado eliminado");
-
-
-            System.out.println("--Listando después de actualizar--");
-            repository.findAll().forEach(System.out::println);
-
-        }
     }
 }
